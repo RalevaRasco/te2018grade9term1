1af37d640761fed7d93a9f435282703808de3277
Code Next <codenext@Codes-MacBook-Air-3.local>
2018-10-06 15:52:22 -0700

    updated math.js

--- math.js
+++ math.js
@@ -11,33 +11,40 @@ console.log(sum);
 // **** Problem 2 ****
 // Store the difference between sum (above) and 7 in a variable.
 // Print the difference.
-
+let diff = sum - 7;
+console.log(diff, "DIFF");
 
 // **** Problem 3 ****
 // Store the product of difference and 3 in a variable.
 // Print the product.
-
+let prod = diff * 3;
+console.log(prod, "PROD")
 
 // **** Problem 4 ****
 // Store the power of the product raised to an exponent of 2 in a variable.
 // Print the power.
-
+let pow = prod ** 2;
+console.log(pow, "POW");
 
 // **** Problem 5 ****
 // Store the quotient of the power divided by 4 in a variable.
 // Print the quotient.
-
+let quot = pow / 4;
+console.log(quot, "QUOT");
 
 // **** Problem 6 ****
 // Store the remainder of the quotient divided by 2 in a variable.
 // Print the remainder.
-
+let rem = quot % 2;
+console.log(rem, "REM");
 
 // **** Problem 7 ****
 // Print a boolean value (true/false) representing if the quotient from
 // problem 5 is even or odd. If the quotient is even, print true. If the
 // quotient is odd, print false.
-//
+let even = rem == 0;
+console.log(even, "EVEN");
+
 // Hint: You can tell if a value is even or odd if it is divisible by 2;
 // in other words, if there is no remainder after dividing the value by
 // 2, then the value is even. See problem 6!
