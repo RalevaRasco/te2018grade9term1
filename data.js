86d5d577cc178c09fbab9ac36bd64bdf5160ff44
Code Next <codenext@Codes-MacBook-Air-3.local>
2018-10-06 13:18:10 -0700

    updated data.js

--- data.js
+++ data.js
@@ -1,5 +1,25 @@
 // Example
-let message = "Hello World!";
+let message = "If water can be wet, can it be dried?";
 console.log(message);
-
-// Type your code below!
+// Number Variables
+let bDayMonth = 12;
+console.log(bDayMonth);
+let bDay = 2;
+console.log(bDay);
+let bDayYear = 2004;
+console.log(bDayYear);
+// String Variables
+let questionOne = "If water can be wet, can it be dried?";
+console.log(questionOne);
+let factssss = "KiD CuDi is underated.";
+console.log(factssss);
+let noice = "*clicks tongue* Noice.";
+console.log(noice);
+// Boolean Variables
+let hasNintendoSwitch = false;
+console.log(hasNintendoSwitch);
+let fifaIsGood = true;
+console.log(fifaIsGood);
+// Undefined variable
+let name;
+console.log(name);
