d4e36fbafa31dc834e3182369a4475d336dc24c6
Code Next <codenext@Codes-MacBook-Air-3.local>
2018-10-20 10:32:35 -0700

    modify files

--- change.js
+++ change.js
@@ -1,7 +1,45 @@
-// import readline-sync module, don't touch!
-const READLINE = require("readline-sync");
+const READLINE = require('readline-sync');
 
-// fill in object below with all relevant properties (see instructions)
-let changeMachine = {};
+// declare and assign a variable for pennies using readlineSync
+// ask the user how many pennies he/she wants change for
+let pennies = READLINE.question('Type in how manny pennies you want.');
 
-// write code below that simulates the change machine in action!
+console.log(pennies);
+
+// declare, assign, and print how many quarters you can make out of the pennies
+let quarters = Math.floor(pennies/25);
+console.log(quarters,'QUARTERS');
+
+// declare, assign, and print how many dimes you can make out of the pennies
+let dimes = Math.floor(pennies%25/10);
+console.log(dimes, 'DIMES');
+
+// declare, assign, and print how many nickels you can make out of the pennies
+let nickels = Math.floor(pennies%25%10/5);
+console.log(nickels,'NICKELS');
+
+// declare, assign, and print how many pennies are left over
+let leftOver = Math.floor(pennies%25%10%5);
+console.log(leftOver, 'LEFTOVER');
+
+// make a change drawer using an object to store all of your change
+// each property on the object should represent the particular coin
+// use the dot operator on the object to assign how many of each coin you have
+// many coins are in the drawer
+// print the drawer to see that the values are correct
+// let changeDrawer = {
+// quartersW: undefined
+// dimesW: undefined
+// nickelsW: undefined
+// penniesW: undefined
+// };
+
+// changeDrawer.quartersW = quarters
+// changeDrawer.dimesW = dimes
+// changeDrawer.nickelsW = nickels
+// changeDrawer.penniesW = pennies
+//
+// console.log($changeDrawer.quartersW,
+// $changeDrawer.dimesW,
+// $changeDrawer.nickelsW,
+// $changeDrawer.penniesW);
---
+++ oblects.js
@@ -0,0 +1,10 @@
+let Jacob = {
+firstName: 'Jacob',
+nickName: 'Jakobe',
+numOfSiblings: 4,
+catchPhrase: 'Thats alot of work.',
+hasNintendoSwitch: false,
+hasXBox: false,
+hasPet: false
+};
+console.log(Jacob.numOfSiblings)
