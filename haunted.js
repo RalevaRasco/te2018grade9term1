e9f951455686fa845dd6a99f870119966fcb3f00
Oscar <oscara@csedge.org>
2018-12-05 20:47:24 -0800

    Update haunted.js

--- haunted.js
+++ haunted.js
@@ -3,9 +3,9 @@ const READLINE = require("readline-sync");
 // use whatever properties you want for the player object
 let player = {
   name: undefined,
-  courage: 0,
-  intellect: 0,
-  strength: 0,
+  courage: 10,
+  intellect: 10,
+  strength: 10,
   charm: 0,
   weapon: "fists",
   item: "flashlight",
@@ -17,15 +17,33 @@ player.name = READLINE.question("What is your name?: ");
 console.log(`${player.name} stands in front of a spooky haunted house.`);
 console.log(`${player.name} hears what sounds like screaming coming from inside the house.`);
 let enterHouse = READLINE.question("Will you enter the house? (yes or no) ");
-if(enterHouse == "y" || enterHouse == "yes") {
+if(enterHouse === "y" || enterHouse === "yes") {
   player.courage++;
   console.log(`${player.name} prepares their ${player.weapon} and opens the door...`);
   // continue the story
+console.log('There is Pennywise and Micheal Myers fighting.')
+let enterDoor = READLINE.question(" Do you go in? (yes or no) ");
+  if(enterDoor === "y" || enterDoor === "yes"){
+  player.courage++;
+  player.intellect--;
+  console.log(`${player.name} decides to enter. However, they get eaten by Pennywise. HAHA LOSER!`);
+    if else(enterDoor === "n" || enterDoor === "no") {
+    player.intellect++;
+      console.log(`${player.name} decides to be smart and doesn't go in.`);
+      console.log(`${player.name} smells a terrible odor and goes to the kitchen. There is a Kitchen Knife.`);
+      let recieveKnife = READLINE.question(" What do you do? a. Get Knife. b. Go to the front door. c. Not move. ");
+        if (recieveKnife === "a" ||recieveKnife === "A"){
+        player.strength++
+        console.log(`${player.name} get decapitated by a knife, you dead. The other answers don't count. ;)`);
+      }
+
+    }
+}
 
 } else {
   player.intellect++;
   console.log(`${player.name} decides not to open the door. However...`);
-  // continue the story
+console.log(`${player.name}, you very smart, you won!!!!`);
 
 }
 console.log("Thanks for playing!");
